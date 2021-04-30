ca4fbd1b4ef80cbc8e32ed91e33561522651aea9
Felipe <72029039+renanz21@users.noreply.github.com>
2021-04-30 03:28:47 +0100

    commit 2

--- java/script.js
+++ java/script.js
@@ -1,4 +1,13 @@
 function validate(){
     var staffPassword = document.getElementById('staffAccess').value;
-    alert(staffPassword);
+
+    //regex check to meet requirements
+    var regx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+
+    if (regx.test(staffPassword)){
+        
+    }
+    else {
+        document.getElementById('invalid').style.visibility= "visible"
+    }
 }
